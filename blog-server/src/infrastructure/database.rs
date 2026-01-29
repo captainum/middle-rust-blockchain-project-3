25adb04d2709ea88ea59aee6f175afdc143d1111
captainum <dmitry.vasilch@gmail.com>
2026-01-29 11:18:30 +0300

    add pg poll creation && apply migrations automatically

---
+++ blog-server/src/infrastructure/database.rs
@@ -0,0 +1,26 @@
+//! Модуль начального взаимодействия с БД.
+
+use std::env;
+use std::time::Duration;
+use sqlx::{postgres::PgPoolOptions, PgPool, migrate};
+
+/// Создать пул соединений.
+pub async fn create_pool() -> anyhow::Result<PgPool> {
+    let database_url = env::var("DATABASE_URL")?;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(20)
+        .min_connections(5)
+        .acquire_timeout(Duration::from_secs(5))
+        .connect(&database_url)
+        .await?;
+
+    Ok(pool)
+}
+
+/// Актуализировать миграции в БД.
+pub async fn run_migrations(pool: &PgPool) -> Result<(), sqlx::Error> {
+    migrate!().run(pool).await?;
+
+    Ok(())
+}
--- blog-server/src/main.rs
+++ blog-server/src/main.rs
@@ -1,9 +1,18 @@
 mod domain;
+mod infrastructure;
+
+use infrastructure::database::{create_pool, run_migrations};
 
 mod blog_grpc {
     tonic::include_proto!("blog");
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+    dotenv::dotenv()?;
+
+    let pool = create_pool().await?;
+    run_migrations(&pool).await?;
+
+    Ok(())
 }
